868cb7aa4bf6bc3fd13469280fa3e4937837a27a
Jakob Hariz <jakob.hariz@gmail.com>
2015-01-28 20:39:26 +0100

    tidying upp

--- obj/MainWindow.d
+++
@@ -1,4 +0,0 @@
-obj/MainWindow.o: src/MainWindow.cpp \
- /home/jh/Desktop/LOL/include/MainWindow.hpp
-
-/home/jh/Desktop/LOL/include/MainWindow.hpp:
--- obj/lab1.d
+++
@@ -1,3 +0,0 @@
-obj/lab1.o: src/lab1.cpp /home/jh/Desktop/LOL/include/testinc.h
-
-/home/jh/Desktop/LOL/include/testinc.h:
